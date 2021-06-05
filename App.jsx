e120e4d6436fab6e9b06cffd62d530177b6a268a
Abhishek Raam <65652675+DsYre@users.noreply.github.com>
2021-06-05 17:22:28 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Note />
+    </div>
+  );
+}
+export default App;
